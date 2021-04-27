a2a20c64cc1401451c03cc74d012a0a0700e0639
yoanndelattre <yoanndelattre21@gmail.com>
2021-04-27 18:49:27 +0200

    add cd docker

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull ubuntu:latest
-docker run -it --rm -v "$PWD:/app" ubuntu:latest sh -c "apt update && apt install make && make build -f /app/Makefile"
+docker run -it --rm -v "$PWD:/app" ubuntu:latest sh -c "apt update && apt install make && cd /app && make build"
 docker rmi ubuntu:latest
