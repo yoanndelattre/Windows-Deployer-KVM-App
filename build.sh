66a5aca81b39bed486a2778ab7ff25e1db03f816
yoanndelattre <yoanndelattre21@gmail.com>
2021-04-27 18:47:46 +0200

    change path makeefile docker

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker pull ubuntu
-docker run -it --rm -v "$PWD:/app" ubuntu:latest sh -c "apt update && apt install make && make build"
-docker rmi ubuntu
+docker pull ubuntu:latest
+docker run -it --rm -v "$PWD:/app" ubuntu:latest sh -c "apt update && apt install make && make build -f /app/Makefile"
+docker rmi ubuntu:latest
