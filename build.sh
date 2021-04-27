e466399f97a205c86802ba2e186eca45725e8124
yoanndelattre <yoanndelattre21@gmail.com>
2021-04-27 20:38:16 +0200

    change base image

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker pull ubuntu:latest
-docker run -it -v "$PWD:/app" ubuntu:latest sh -c "apt update && apt install -y make && make -f /app/Makefile build"
-#docker rmi ubuntu:latest
+docker pull golang:latest
+docker run -it --rm -v "$PWD:/app" golang:latest sh -c "apt update && apt install -y make && make -f /app/Makefile build"
+docker rmi golang:latest
