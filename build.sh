617b7b5854616becce544defdba03786c3025e95
yoanndelattre <yoanndelattre21@gmail.com>
2021-04-27 18:51:32 +0200

    add workdir

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull ubuntu:latest
-docker run -it --rm -v "$PWD:/app" ubuntu:latest sh -c "apt update && apt install make && cd /app && make build"
+docker run -it --rm -v "$PWD:/app" --workdir="/app" ubuntu:latest sh -c "apt update && apt install make && make build"
 docker rmi ubuntu:latest
